6e224d0fd741f10ff10e9959f23c1d2373eda5be
gooldon <ldon@google.com>
2022-10-14 16:51:58 +0200

    initial deployment

---
+++ deploy/orgrole/main.tf
@@ -0,0 +1,23 @@
+#custom role IAM assignment test
+
+
+
+#Org Policy Success Scenario
+
+resource "google_organization_iam_binding" "orgtfdeploy" {
+  org_id  = "716543262869"
+  role    = "organizations/716543262869/roles/custom_role_org_s"
+  members = [
+      "serviceAccount:customrolesa@tfdeploy-221013.iam.gserviceaccount.com",
+    ]
+}
+
+#Org Policy Failure Scenario
+
+resource "google_organization_iam_binding" "orgtfdeploy" {
+  org_id  = "716543262869"
+  role    = "organizations/716543262869/roles/custom_role_org_f"
+  members = [
+      "serviceAccount:customrolesa@tfdeploy-221013.iam.gserviceaccount.com",
+    ]
+}
---
+++ deploy/projrole/main.tf
@@ -0,0 +1,21 @@
+#custom role IAM assignment test
+
+#Project IAM Policy Success Scenario
+
+resource "google_project_iam_binding" "projtfdeploy" {
+  project = "tfdeploy-221013"
+  role    = "projects/tfdeploy-221013/roles/custom_role_proj_s"
+  members = [
+      "serviceAccount:customrolesa@tfdeploy-221013.iam.gserviceaccount.com",
+    ]
+}
+
+#Project IAM Policy Failure Scenario
+
+resource "google_project_iam_binding" "projtfdeploy" {
+  project = "tfdeploy-221013"
+  role    = "projects/tfdeploy-221013/roles/custom_role_proj_f"
+  members = [
+      "serviceAccount:customrolesa@tfdeploy-221013.iam.gserviceaccount.com",
+    ]
+}
