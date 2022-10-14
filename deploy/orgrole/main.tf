b699d5f2e254e3741dee01fd6a3e99ad70b0c283
gooldon <ldon@google.com>
2022-10-14 18:37:02 +0200

    failure test

--- deploy/orgrole/main.tf
+++ deploy/orgrole/main.tf
@@ -5,20 +5,20 @@ provider "google" {
 
 #Org Policy Success Scenario
 
-resource "google_organization_iam_binding" "orgtfdeploy_success" {
-  org_id  = "716543262869"
-  role    = "organizations/716543262869/roles/custom_role_org_s"
-  members = [
-      "serviceAccount:customrolesa@tfdeploy-221013.iam.gserviceaccount.com",
-    ]
-}
-
-#Org Policy Failure Scenario
-
-#resource "google_organization_iam_binding" "orgtfdeploy_failure" {
+#resource "google_organization_iam_binding" "orgtfdeploy_success" {
 #  org_id  = "716543262869"
-#  role    = "organizations/716543262869/roles/custom_role_org_f"
+#  role    = "organizations/716543262869/roles/custom_role_org_s"
 #  members = [
 #      "serviceAccount:customrolesa@tfdeploy-221013.iam.gserviceaccount.com",
 #    ]
 #}
+
+#Org Policy Failure Scenario
+
+resource "google_organization_iam_binding" "orgtfdeploy_failure" {
+  org_id  = "716543262869"
+  role    = "organizations/716543262869/roles/custom_role_org_f"
+  members = [
+      "serviceAccount:customrolesa@tfdeploy-221013.iam.gserviceaccount.com",
+    ]
+}
